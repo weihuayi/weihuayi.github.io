02806040adf0f1be7ce18526522ad2f980add622
Huayi Wei <weihuayi@xtu.edu.cn>
2019-02-18 08:57:20 +0800

    update

--- gitbook.sh
+++ gitbook.sh
@@ -1,11 +1,11 @@
 git checkout master
 git add .
-git commit -m $1
+git commit -m "update" 
 git push 
 gitbook build
 cp -r _book/* ../weihuayi.github.io/
 cd ../weihuayi.github.io/
 git add .
-git commit -m $1
+git commit -m "update" 
 git push 
 git checkout master
