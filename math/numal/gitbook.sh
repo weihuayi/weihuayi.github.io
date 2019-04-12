6771145aa23a676e92e0c9d9526d41f385a7d7ac
Huayi Wei <weihuayi@xtu.edu.cn>
2019-04-12 09:15:10 +0800

    update

--- math/numal/gitbook.sh
+++ math/numal/gitbook.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
-CHANGED=$(git diff-index --name-only HEAD --)
-if [ ! -z $CHANGED ];then
-    cd ../weihuayi.github.io/
-    git pull
-    cd ../numal/
-    git add .
-    git commit -m "update" 
-    git pull
-    git push 
-    gitbook build
-    cp -r _book/* ../weihuayi.github.io/math/numal/
-    cd ../weihuayi.github.io/
-    git add .
-    git commit -m "update" 
-    git push 
-fi
+cd ../weihuayi.github.io/
+git pull
+
+cd ../numal/
+git add .
+git commit -m "update" 
+git push 
+gitbook build
+cp -r _book/* ../weihuayi.github.io/math/numal/
+cd ../weihuayi.github.io/
+git add .
+git commit -m "update" 
+git push 
