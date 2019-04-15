2418727872d8f26a69db788ac0f8ad4076fe426b
Huayi Wei <weihuayi@xtu.edu.cn>
2019-04-15 15:19:15 +0800

    update

--- math/numal/gitbook.sh
+++ math/numal/gitbook.sh
@@ -1,15 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-cd ../weihuayi.github.io/
-git pull
-
-cd ../numal/
-git add .
-git commit -m "update" 
-git push 
-gitbook build
-cp -r _book/* ../weihuayi.github.io/math/numal/
-cd ../weihuayi.github.io/
-git add .
-git commit -m "update" 
-git push 
+if [[ `git status --porcelain` ]];then
+    cd ../weihuayi.github.io/
+    git pull
+    cd ../numal/
+    git add .
+    git commit -m "update" 
+    git pull
+    git push 
+    gitbook build
+    cp -r _book/* ../weihuayi.github.io/math/numal/
+    cd ../weihuayi.github.io/
+    git add .
+    git commit -m "update" 
+    git push 
+fi
