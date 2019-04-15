90d50117ef665996ac39bd7112542dd828a6a150
Huayi Wei <weihuayi@xtu.edu.cn>
2019-04-15 15:12:59 +0800

    update

--- math/numopde/gitbook.sh
+++ math/numopde/gitbook.sh
@@ -1,9 +1,15 @@
-git add .
-git commit -m "update" 
-git push 
-gitbook build
-cp -r _book/* ../weihuayi.github.io/math/numopde/
-cd ../weihuayi.github.io/
-git add .
-git commit -m "update" 
-git push 
+if [[ `git status --porcelain` ]]; then
+    cd ../weihuayi.github.io/
+    git pull
+    cd ../numopde/
+    git add .
+    git commit -m "update" 
+    git pull
+    git push 
+    gitbook build
+    cp -r _book/* ../weihuayi.github.io/math/numopde/
+    cd ../weihuayi.github.io/
+    git add .
+    git commit -m "update" 
+    git push 
+fi
