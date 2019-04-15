fdb8a8ba738af9c93e4c9109ea790ae1810aeebe
Huayi Wei <weihuayi@xtu.edu.cn>
2019-04-15 15:12:42 +0800

    update

--- gitbook.sh
+++ gitbook.sh
@@ -1,15 +1,16 @@
 # update the site first
-cd ../weihuayi.github.io/
-git pull
-
-
-cd ../whysite/
-git add .
-git commit -m "update" 
-git push 
-gitbook build
-cp -r _book/* ../weihuayi.github.io/
-cd ../weihuayi.github.io/
-git add .
-git commit -m "update" 
-git push 
+if [[ `git status --porcelain` ]]; then
+    cd ../weihuayi.github.io/
+    git pull
+    cd ../whysite/
+    git add .
+    git commit -m "update" 
+    git pull
+    git push 
+    gitbook build
+    cp -r _book/* ../weihuayi.github.io/
+    cd ../weihuayi.github.io/
+    git add .
+    git commit -m "update" 
+    git push 
+fi
